0f6f959e44a41c636bf9c6e476a84893dbd861d5
Shiina Yuri <yurishouse@gmail.com>
2020-10-26 13:44:36 -0700

    update

--- MenusAndStuff/app/src/main/java/com/example/menusandstuff/MainActivity.java
+++ MenusAndStuff/app/src/main/java/com/example/menusandstuff/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.menusandstuff;
 // made by Zhongli Liang
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -34,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         TextView time_view = findViewById(R.id.time_text);
         date_view.setText("Date: "+date_formatted);
         time_view.setText("Time: "+time_formatted);
+
     }
 
     @Override
@@ -90,5 +96,59 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
-}
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),"datePicker");
+    }
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = ("Date: "+ year_string+ "-" +month_string  + "-" +day_string );
+        TextView Date_text = findViewById(R.id.date_text);
+        Date_text.setText(dateMessage);
+    }
+    public void showTimePicker(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(),
+                getString(R.string.set_time));
+
+    }
+    /**
+     * Process the time picker result into strings that can be displayed in
+     * a Toast.
+     *
+     * @param hourOfDay Chosen hour
+     * @param minute Chosen minute
+     */
+    public void processTimePickerResult(int hourOfDay, int minute) {
+        // Convert time elements into strings.
+        String hour_string = Integer.toString(hourOfDay);
+        String minute_string = Integer.toString(minute);
+        // Assign the concatenated strings to timeMessage.
+        String timeMessage = ("Time: "+hour_string + ":" + minute_string);
+        TextView Time_Text = findViewById(R.id.time_text);
+        Time_Text.setText(timeMessage);
+    }
+    public void exitDialog(View view) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Exit");
+        builder.setMessage("Exit now?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i){finish();}
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i){
+                Toast toast = Toast.makeText(MainActivity.this, R.string.exit_text , Toast.LENGTH_SHORT);
+                toast.show();
+                dialogInterface.dismiss()
+                ;}
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    }
 
---
+++ MenusAndStuff/app/src/main/java/com/example/menusandstuff/TimePickerFragment.java
@@ -0,0 +1,62 @@
+package com.example.menusandstuff;
+// made by Zhongli Liang
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass for the time picker.
+ */
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+
+    /**
+     * Creates the time picker dialog with the current time from Calendar.
+     *
+     * @param savedInstanceState    Saved instance state bundle.
+
+     * @return TimePickerDialog     The time picker dialog.
+     */
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it.
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+
+    /**
+     * Grabs the time and converts it to a string to pass
+     * to the Main Activity in order to show it with processTimePickerResult().
+     *
+     * @param timePicker    The time picker view
+     * @param hourOfDay     The hour chosen
+     * @param minute        The minute chosen
+     */
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+        MainActivity activity = (MainActivity) getActivity();
+        activity.processTimePickerResult(hourOfDay, minute);
+    }
+}
